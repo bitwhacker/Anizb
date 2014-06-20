4fb1347d37b940b03ee1d0f2ad2f2aeee4ba3f51
Brad Beimer <Brad.Beimer@statestreet.com>
2014-06-20 14:09:14 -0700

    Naming changes

--- anizb.py
+++ anizb.py
@@ -1,7 +1,7 @@
 """ Main routine - just tests for libraries at this point"""
 from flask import Flask
 from lib.py3utils import Config
-from lib.py3anidb import *
+from lib.py3anidb import Anidb
 
 app = Flask(__name__)
 
@@ -12,24 +12,15 @@ def hello():
 if __name__ == '__main__':    
 #    app.run()
     config = Config()
-    anizb = config.get_section('anizb', {
+    anizb = config.set_default('anizb', {
         'dbtype' : 'sqlite',
         'port' : '3306',
         'hostname' : 'localhost',
         'user' : 'anizb',
         'password' : 'none',
         'dbname' : 'anizb'})
-    anidb = config.get_section('anidb', {
-        'dbtype' : 'sqlite',
-        'port' : '3306',
-        'hostname' : 'localhost',
-        'user' : 'anidb',
-        'password' : 'none',
-        'dbname' : 'anidb'})
-    model = AnidbModel()
 
-    ts = TitleSearch()
-#    ts.load_titles()
-#    for title in m.get_titles():
-#        print (title.anidbid, title.title, title.language, title.type)
-    ts.search('One Piece')
+    anidb = Anidb()
+    titles = anidb.get_anime_titles_by_substring('One Piece')
+    for title in titles:
+        print(title.anidbid, title.title)
--- test.py
+++
@@ -1,7 +0,0 @@
-import os.path
-import sys
-import lib.py3utils as py3utils
-import lib.py3anidb as py3anidb
-
-target_metadata = py3anidb.anidbmodel.Base.metadata
-print(target_metadata)
